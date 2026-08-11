4c3ddb084f331a5d5668a88a9dfc6dcdbdbbe923
kaua03 <125662694+kaua03@users.noreply.github.com>
2026-08-11 13:48:23 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,21 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Como combinamos, a URL do banco está exposta por enquanto. 
-# Depois vamos esconder isso no .env!
-URL_DO_BANCO = "postgresql://usuario:senha@localhost:5432/datawallet"
+# 1. Abre o arquivo secreto .env e decora as senhas na memória
+load_dotenv()
 
-# O "engine" é o motor que mantém a conexão aberta
+# 2. Pega a Chave Mestra que configuramos lá
+URL_DO_BANCO = os.getenv("DATABASE_URL")
+
+# 3. O Motor: Ele quem gerencia a estrada até o Supabase
 engine = create_engine(URL_DO_BANCO)
 
-# A "SessionLocal" é a catraca de entrada. Cada vez que um usuário
-# acessa o app, abrimos uma sessão para ele fazer consultas.
+# 4. A Catraca: Toda vez que o celular mandar um dado, 
+# abrimos uma "sessão" temporária e depois fechamos, para não travar o banco.
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# A "Base" é o molde que usaremos para criar as nossas tabelas em Python
+# 5. O Molde de Gesso: Usaremos isso no próximo arquivo para 
+# esculpir o formato exato das nossas tabelas (Categorias, Contas, etc)
 Base = declarative_base()
