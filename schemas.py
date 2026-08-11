a51ee75e4c531243c14ecbc8fb542885db3afecb
kaua03 <125662694+kaua03@users.noreply.github.com>
2026-08-11 15:26:34 -0300

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,24 +1,49 @@
 from pydantic import BaseModel
-from typing import Optional
 from datetime import date
+from typing import Optional
 
 # ==========================================
-# SCHEMAS DE ENTRADA (O que o Celular envia)
+# 1. SCHEMAS DE USUÁRIO (A Porta de Entrada)
 # ==========================================
+class UsuarioLogin(BaseModel):
+    email: str
+    senha: str
 
+class UsuarioResposta(BaseModel):
+    id: int
+    nome: str
+    email: str
+
+    class Config:
+        from_attributes = True
+
+# ==========================================
+# 2. SCHEMAS DE CATEGORIA
+# ==========================================
+class CategoriaCriar(BaseModel):
+    nome: str
+    icone: str
+    cor: str
+
+class CategoriaResposta(BaseModel):
+    id: int
+    nome: str
+    icone: str
+    cor: str
+    usuario_id: int
+
+    class Config:
+        from_attributes = True
+
+# ==========================================
+# 3. SCHEMAS DE TRANSAÇÃO (Aba Início)
+# ==========================================
 class TransacaoCriar(BaseModel):
     valor: float
-    tipo: str
+    tipo: str # 'receita' ou 'despesa'
     descricao: str
     data_vencimento: date
-    
-    # IDs para amarrar a qual usuário e categoria essa conta pertence
-    usuario_id: int
     categoria_id: int
-
-# ==========================================
-# SCHEMAS DE SAÍDA (O que a API devolve para o Celular)
-# ==========================================
 
 class TransacaoResposta(BaseModel):
     id: int
@@ -26,10 +51,49 @@ class TransacaoResposta(BaseModel):
     tipo: str
     descricao: str
     data_vencimento: date
-    status: str
-    
     categoria_id: int
+    usuario_id: int
 
     class Config:
-        # Isso ensina o Inspetor a ler os dados direto do SQLAlchemy
+        from_attributes = True
+
+# ==========================================
+# 4. SCHEMAS DE DÍVIDAS (O Kanban)
+# ==========================================
+class DividaCriar(BaseModel):
+    descricao: str
+    valor: float
+    data_vencimento: date
+
+class DividaResposta(BaseModel):
+    id: int
+    descricao: str
+    valor: float
+    data_vencimento: date
+    status: str
+    usuario_id: int
+
+    class Config:
+        from_attributes = True
+
+# ==========================================
+# 5. SCHEMAS DE PLANOS (Metas e Gamificação)
+# ==========================================
+class PlanoCriar(BaseModel):
+    nome: str
+    valor_meta: float
+    cor: str
+
+class PlanoAporte(BaseModel):
+    valor: float # Usado apenas para quando você for injetar dinheiro na meta
+
+class PlanoResposta(BaseModel):
+    id: int
+    nome: str
+    valor_atual: float
+    valor_meta: float
+    cor: str
+    usuario_id: int
+
+    class Config:
         from_attributes = True
