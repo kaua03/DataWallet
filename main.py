f92b44a7f83e8fb4da052799b690d3fd087bdcbc
kaua03 <125662694+kaua03@users.noreply.github.com>
2026-08-11 13:32:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 import database
+import models
+import schemas # Importando nossa alfândega
 
-# Aqui nós dizemos: "Pegue todos os moldes e crie as tabelas no banco de dados"
-database.Base.metadata.create_all(bind=database.engine)
+# Cria as tabelas lá no Supabase (se já não existirem)
+models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI(title="DataWallet API")
 
-@app.get("/")
-def rota_principal():
-    return {"mensagem": "DataWallet operando com banco de dados conectado!"}
+# Isso é o porteiro do banco de dados. Ele abre a catraca quando uma 
+# requisição chega e fecha quando ela vai embora.
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# ROTA DE CRIAÇÃO (POST): Aqui nós enviamos novos dados
+@app.post("/transacoes/", response_model=schemas.TransacaoResposta)
+def criar_transacao(transacao_entrada: schemas.TransacaoCriar, db: Session = Depends(get_db)):
+    # Pegamos o JSON validado e transformamos no objeto do nosso models.py
+    nova_transacao = models.Transacao(
+        valor=transacao_entrada.valor,
+        tipo=transacao_entrada.tipo,
+        descricao=transacao_entrada.descricao,
+        data_vencimento=transacao_entrada.data_vencimento,
+        usuario_id=transacao_entrada.usuario_id,
+        categoria_id=transacao_entrada.categoria_id
+    )
+    
+    # Colocamos na esteira de salvamento
+    db.add(nova_transacao)
+    # Commit = Apertar o botão verde que diz "Pode Salvar Definitivamente!"
+    db.commit()
+    # Atualiza a nova_transacao com o ID gerado lá no Supabase
+    db.refresh(nova_transacao) 
+    
+    return nova_transacao
+
+# ROTA DE LEITURA (GET): Aqui nós buscamos as informações
+@app.get("/transacoes/")
+def listar_transacoes(db: Session = Depends(get_db)):
+    # Pede pro banco: "Me dê TODAS as linhas da tabela transacoes"
+    contas = db.query(models.Transacao).all()
+    return contas
