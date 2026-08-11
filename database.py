77569e9c4bc5f7ca6e137791dbde7c8c101b7c4d
kaua03 <125662694+kaua03@users.noreply.github.com>
2026-08-11 15:57:13 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,21 +1,18 @@
-import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# 1. Abre o arquivo secreto .env e decora as senhas na memória
-load_dotenv()
+# ==========================================
+# CONEXÃO DIRETA COM O SUPABASE
+# Substitua o texto abaixo pela sua URL do Supabase!
+# ==========================================
+SQLALCHEMY_DATABASE_URL = "https://aoeyeleaxbwvjmzxxdib.supabase.co"
 
-# 2. Pega a Chave Mestra que configuramos lá
-URL_DO_BANCO = os.getenv("DATABASE_URL")
+# O motor que conecta o Python ao PostgreSQL do Supabase
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-# 3. O Motor: Ele quem gerencia a estrada até o Supabase
-engine = create_engine(URL_DO_BANCO)
-
-# 4. A Catraca: Toda vez que o celular mandar um dado, 
-# abrimos uma "sessão" temporária e depois fechamos, para não travar o banco.
+# A fábrica de sessões (como o Python conversa com o banco)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# 5. O Molde de Gesso: Usaremos isso no próximo arquivo para 
-# esculpir o formato exato das nossas tabelas (Categorias, Contas, etc)
+# A classe base para nossos modelos
 Base = declarative_base()
